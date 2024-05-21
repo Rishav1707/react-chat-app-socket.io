a779825a48767dae7fb98038860494fe66bba42a
Rishav Raj <rishavraj1707music@gmail.com>
2024-05-22 00:37:55 +0530

    fix: Minor change

--- frontend/src/components/VideoCall.jsx
+++ frontend/src/components/VideoCall.jsx
@@ -20,6 +20,7 @@ const VideoCall = ({ peerInstance, currentUserVideoRef, remoteVideoRef }) => {
         });
       });
     });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
