c8e84724867f4170a325bd6e220c0413f8cde3ff
Rishav Raj <rishavraj1707music@gmail.com>
2024-05-22 22:32:57 +0530

    fix: CSS of the request component

--- frontend/src/components/RequestPopUp.jsx
+++ frontend/src/components/RequestPopUp.jsx
@@ -1,7 +1,17 @@
+import { useEffect, useState } from "react";
 import { socket } from "../utils/createSocketHost";
+import { userById } from "../utils/backendRequest";
 
 /* eslint-disable react/prop-types */
 const RequestPopUp = ({ setRequestId, to, from, peerId, setOpenVideoCall }) => {
+  const [toUser, settoUser] = useState({});
+
+  useEffect(() => {
+    userById(to).then((data) => {
+      settoUser(data);
+    });
+  }, [to]);
+
   const handleCallDecline = () => {
     setRequestId(null);
     setOpenVideoCall(false);
@@ -16,9 +26,13 @@ const RequestPopUp = ({ setRequestId, to, from, peerId, setOpenVideoCall }) => {
 
   return (
     <div className="RequestPopUpContainer">
-      <h3>I want your Video and Audio access</h3>
-      <button onClick={handleCallAccept}>Accept</button>
-      <button onClick={handleCallDecline}>Decline</button>
+      <div>
+        <h3>{toUser.firstName} want&apos;s your Video and Audio access</h3>
+        <div className="RequestButton">
+          <button onClick={handleCallAccept}>Accept</button>
+          <button onClick={handleCallDecline}>Decline</button>
+        </div>
+      </div>
     </div>
   );
 };
